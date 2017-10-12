49d282b1ff4b5344ed0982a65adb5c97e728a972
Colton Booth <32721476+maritimerabroad@users.noreply.github.com>
2017-10-12 17:00:04 -0400

    Updated to add tkinter as temporary GUI

--- AGI-Calculator.py
+++ AGI-Calculator.py
@@ -1,18 +1,24 @@
 #AGI Calculator  
 #For calculating tenant rent amounts with various 
 #above guideline increases, and city rent reductions.
-#
+
 #This is released under the GPL V2
 
 
+
+#importing stuff for GUI
+import tkinter
+top = tkinter.Tk()
+#code to add widgets will go here
+top.mainloop()
+
+
+
 #Enter guideline amounts, agi's and rent reductions (if any)
-print("Welcome to AGI-Calculator")
-guideline = float(input("Please enter this year's guideline amount:"))
-AGI1 = float(input("Please enter the approved AGI amount for the first year:  "))
-AGI2 = float(input("Please enter the approved AGI amount for the second year:  "))
-AGI3 = float(input("Please enter the approved AGI amount for the third year:  "))
-reduction = float(input("Please enter the city rent reduction for this year:  "))
-currentRent = float(input("Please enter the tenant's current rent:  "))
-
-
+guideline = 0
+AGI1 = 0
+AGI2 = 0
+AGI3 = 0
+reduction = 0
+currentRent = 0
 
